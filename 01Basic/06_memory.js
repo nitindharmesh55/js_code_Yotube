eeb8dabf814db102495fa9c07bcd1e932b98b263
Nitin <nitindharmesh01@gmail.com>
2025-08-19 05:50:31 +0000

    stack and heap

---
+++ 01Basic/06_memory.js
@@ -0,0 +1,8 @@
+// Stack and heap memory;
+// 1. STACK : ALL the primitive data type use stack memory;
+// in stack memory we got the copy of value;
+
+// 2. Heap : used by all non primtive type
+// Where they got the reference of the real value means we can change the real value;
+
+let myYoutubeName = "NitinDharmesh";
