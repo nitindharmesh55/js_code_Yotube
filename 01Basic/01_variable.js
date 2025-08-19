bfe8b7da4c4188de4a68260918a2d62b78f61e53
Nitin <nitindharmesh01@gmail.com>
2025-08-19 04:08:09 +0000

    variable

---
+++ 01Basic/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = 14663;
+let accountEmail = "nitin@google.com"
+var accountPassword = "12345"
+accountCity = "japan"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
