6cc3c0a10abd05615d83e99b6605bee13a5759db
Nitin <nitindharmesh01@gmail.com>
2025-08-19 04:08:42 +0000

    Datatypes for strict

---
+++ 01Basic/02_datatype.js
@@ -0,0 +1,18 @@
+// "Use Strict"// treat all js coe as newer version!
+// code readability should be high
+
+let name = "niitn"; // string;
+
+let age = 22;// number;
+
+let login = true;// boolean;
+
+// Null : stand alone value; // type of null is object
+
+// undefined => don't have value yet!// type of this is undefined;;
+
+
+// symbol => uqniue 
+
+
+// Object
