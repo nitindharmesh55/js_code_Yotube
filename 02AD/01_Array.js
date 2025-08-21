f5af9a04b3f08972dd9c3d72c0cdfd9f8cac01a3
Nitin <nitindharmesh01@gmail.com>
2025-08-21 04:35:50 +0000

    Part 1 Array (inlude Slice and Splice)

---
+++ 02AD/01_Array.js
@@ -0,0 +1,16 @@
+const myArray = [ 0,1,2,3,4,5];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+
+// 
+let myHeros = ["Spiderman", "Superman", "Nova"];
+let newArray = new Array(1,2,3,4,5);
+console.log(newArray);
+
+
+
+// When you use the slice it will remove the element but the orginal array will stay as it is ;
+// when you use the splice it will remove the element inlcudeing range but it also remove those elements from the origianl array!
+
