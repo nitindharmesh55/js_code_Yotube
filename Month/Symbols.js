44cdd0040ad7930d15905101c7bf89d8998a925c
Nitin <nitindharmesh01@gmail.com>
2025-08-27 02:55:43 +0000

    Symbolspowees

---
+++ Month/Symbols.js
@@ -0,0 +1,10 @@
+ const mysm = Symbol("My custom symbols");
+ console.log(mysm);
+ 
+
+//  Comparing Symbols;
+const symbol1 =  Symbol();
+const Symbol2 = Symbol();
+
+
+console.log(Symbol2 === symbol1); // false
