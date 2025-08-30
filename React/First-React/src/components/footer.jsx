de5a01855fb3c9b38375dd470bd45792ebf23b33
Nitin <nitindharmesh01@gmail.com>
2025-08-30 03:20:07 +0000

    Footer for React

---
+++ React/First-React/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+        <p>All rights are reseverd for  @NitinDharmesh 2025</p>
+    </footer>
+  )
+}
+
+export default footer
