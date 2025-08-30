b02543338cfbc2c759f9880dd0000a071a45937e
Nitin <nitindharmesh01@gmail.com>
2025-08-30 03:42:18 +0000

    Practice  for JSX

---
+++ React/First-React/src/components/welcome.jsx
@@ -0,0 +1,10 @@
+const Welcome = ()=>{
+
+return <div>
+    <h1>Welcome To My ReactSite</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto, labore?</p>
+</div>
+
+
+}
+export default Welcome;
