eb9ab8707877c59f84e0468160cb6314fb5c42a1
Nitin <nitindharmesh01@gmail.com>
2025-08-30 03:26:53 +0000

    Main Content

---
+++ React/First-React/src/components/MainContent.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <div>
+   <h2>Main Content start from here</h2>
+   <h3>About NitinDharmesh</h3>
+
+   <p>
+  Hi, I'm Nitin Dharmesh — a passionate learner and aspiring software engineer. 
+  I focus on web development, UI/UX design, and constantly improving my skills 
+  in computer science, math, and AI. My goal is to build meaningful projects, 
+  grow stronger every day, and create a future based on my own dreams.
+</p>
+
+
+
+
+    </div>
+  )
+}
+
+export default MainContent
