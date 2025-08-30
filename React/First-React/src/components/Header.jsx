dfc74cd1f7a014b387a7066de0af65b465a0646b
Nitin <nitindharmesh01@gmail.com>
2025-08-30 03:19:55 +0000

    Header for react

---
+++ React/First-React/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () =>{
+    return <header>
+        <h1>NitinDharmesh</h1>
+        <nav>
+            <a href="#Home">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#About">About</a>
+        </nav>
+    </header>
+}
+
+export default Header;
